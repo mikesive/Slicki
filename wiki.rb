27ad089d1d42a937c6ea7e117a96e286a1e7a0e6
Michael Sive <mikesive@gmail.com>
2015-12-23 19:24:56 +1000

    eval strings

--- wiki.rb
+++ wiki.rb
@@ -1,12 +1,12 @@
 #!/usr/bin/env ruby
 require 'wikipedia'
 require 'sinatra'
-search = ARGV.join(" ")
-page = Wikipedia.find(search)
-puts page.text
 
 post '/wiki' do
   search_param = params['text']
   result = Wikipedia.find(search_param)
-  "result.title\nresult.content\nresult.fullurl"
+  "#{result.title}\n
+  ----------------------------------------------\n
+  #{result.content}\n\n
+  #{result.fullurl}"
 end
